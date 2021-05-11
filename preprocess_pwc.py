3b47dc82ca6604f983b30f96a3517047a983975d
davin <deyong1994@163.com>
2021-05-11 16:24:41 +0800

    add model

--- preprocess_pwc.py
+++ preprocess_pwc.py
@@ -1,10 +1,13 @@
+import argparse
+
 import cv2
 import numpy as np
 import torch
 import torchvision.transforms as T
+from tqdm import tqdm
+
 from cnn.PWC import pwc_dc_net
 from preprocess_farneback import (change_brightness, generate_frames)
-from tqdm import tqdm
 
 device = torch.device('cuda')
 model = pwc_dc_net('./trained_models/pwc/pwc_net.pth.tar').to(device)
@@ -14,7 +17,7 @@ model.eval()
 def transform(image, bright_factor):
     """augment brightness, crop/resize"""
     image = change_brightness(image, bright_factor)
-    image = cv2.resize(image[100:440, :-90], (192, 64), interpolation = cv2.INTER_AREA)
+    image = cv2.resize(image[100:440, :-90], (192, 64), interpolation=cv2.INTER_AREA)
     return T.ToTensor()(image)
 
 
@@ -42,7 +45,8 @@ def pwc_calc_dense_optical_flow(prev_frame, curr_frame, bright_factor):
 
 def generate_optical_flow_dataset_pwc(mp4_path, text_path):
     """generate dataset from mp4 and txt"""
-    for t, (prev_frame, curr_frame) in enumerate(tqdm(generate_frames(mp4_path), desc='Generating dense optical flow tensors')):
+    for t, (prev_frame, curr_frame) in enumerate(
+            tqdm(generate_frames(mp4_path), desc='Generating dense optical flow tensors')):
         bright_factor = 0.2 + np.random.uniform()
         flow_tensor = pwc_calc_dense_optical_flow(prev_frame, curr_frame, bright_factor)
         if t == 0:
@@ -56,6 +60,19 @@ def generate_optical_flow_dataset_pwc(mp4_path, text_path):
     return flow_dataset
 
 
+def generate_optical_flow_dataset_pwc_by_video(mp4_path):
+    """generate dataset from mp4 and txt"""
+    for t, (prev_frame, curr_frame) in enumerate(
+            tqdm(generate_frames(mp4_path), desc='Generating dense optical flow tensors')):
+        bright_factor = 0.2 + np.random.uniform()
+        flow_tensor = pwc_calc_dense_optical_flow(prev_frame, curr_frame, bright_factor)
+        if t == 0:
+            flow_stack = flow_tensor
+        else:
+            flow_stack = torch.cat([flow_stack, flow_tensor])
+    return torch.utils.data.TensorDataset(flow_stack)
+
+
 def save_whole_set(mp4_path, text_path, save_path):
     """save whole dataset"""
     flow_dataset = generate_optical_flow_dataset_pwc(mp4_path, text_path)
@@ -63,4 +80,10 @@ def save_whole_set(mp4_path, text_path, save_path):
 
 
 if __name__ == '__main__':
-    save_whole_set('./data/train/train.mp4', './data/train/train.txt', '/freespace/local/ajd311/pwc_train.pt')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_video", help="input video path")
+    parser.add_argument("--out_dataset", help="output dataset path")
+
+    args = parser.parse_args()
+
+    save_whole_set(args.input_video, './data/train/train.txt', args.out_dataset)
